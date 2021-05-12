b617ae579d0b553c4ba541b5378589a8d783a36b
Jordan Montero <jordanmontero@Jordans-MBP.attlocal.net>
2021-05-12 13:48:18 -0700

    Nav done. Working on results

--- hulu-clone/components/Nav.js
+++ hulu-clone/components/Nav.js
@@ -1,6 +1,8 @@
 import requests from "../utils/requests";
+import { useRouter } from 'next/router';
 
 function Nav() {
+  const router = useRouter();
   return (
     <nav className='relative'>
       {/* padding on the x axis is 10, whitespace-nowrap keeps top rated from wraping under, space-x-10 puts space between each title*/}
@@ -9,6 +11,8 @@ function Nav() {
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
             key={key}
+            // This onClick is same as "Link to=/genre/actionmovies"
+            onClick={() => router.push(`/?genre=${key}`) }
             className="last:pr-24 cursor-pointer transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
           >
             {title}
